94df217acbddf37df3eabab3baf852caeed5de49
Roshanacharya19 <121121281+Roshanacharya19@users.noreply.github.com>
2023-12-11 20:47:45 -0600

    Fix for not saving the corssed item

--- public/js/script.js
+++ public/js/script.js
@@ -26,18 +26,49 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-// Sync crossed-out state between tabs/windows
-window.addEventListener('storage', (event) => {
-  if (event.key === 'crossedOutItems') {
-    const crossedOutItems = JSON.parse(event.newValue);
-    listItems.forEach((listItem) => {
-      listItem.classList.remove('crossed');
-    });
-    crossedOutItems.forEach((text) => {
-      const listItem = Array.from(listItems).find((item) => item.textContent === text);
-      if (listItem) {
-        listItem.classList.add('crossed');
+// Sync crossed-out items across devices using a backend service
+// Please note: You'll need to implement API endpoints to handle these requests
+
+// Function to update the server with the latest crossed-out items
+function updateCrossedOutItemsOnServer(crossedOutItems) {
+  fetch('/updateCrossedOutItems', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ crossedOutItems }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (!data.success) {
+      console.error('Failed to update items on the server.');
+    }
+  })
+  .catch(error => {
+    console.error('Error updating crossed-out items:', error);
+  });
+}
+
+// Function to get the crossed-out items from the server when the page loads
+function getCrossedOutItemsFromServer() {
+  fetch('/getCrossedOutItems')
+    .then(response => response.json())
+    .then(data => {
+      if (data.crossedOutItems) {
+        data.crossedOutItems.forEach((text) => {
+          const listItem = Array.from(listItems).find((item) => item.textContent === text);
+          if (listItem) {
+            listItem.classList.add('crossed');
+          }
+        });
       }
+    })
+    .catch(error => {
+      console.error('Error fetching crossed-out items:', error);
     });
-  }
+}
+
+// Call the function to sync crossed-out items on page load
+window.addEventListener('DOMContentLoaded', () => {
+  getCrossedOutItemsFromServer();
 });
