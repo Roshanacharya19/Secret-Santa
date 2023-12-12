374d0ac318b3248693f6ff5b5f5c7c8542bf45ec
Roshan Acharya <rosan_acharya@vfc.comm>
2023-12-11 22:09:40 -0600

    Change in scritp

--- public/js/script.js
+++ public/js/script.js
@@ -1,74 +1,51 @@
-// Get all the list items
-const listItems = document.querySelectorAll('li');
+// Get the list element
+var list = document.getElementById('list');
 
-// Add click event listener to each list item
-listItems.forEach((listItem) => {
-  listItem.addEventListener('click', () => {
-    // Toggle the 'crossed' class on the clicked list item
-    listItem.classList.toggle('crossed');
+// Add click event listener to the list
+list.addEventListener('click', function (event) {
+    // Check if the clicked element is an <li> inside the list
+    if (event.target.tagName === 'LI') {
+        // Toggle the 'crossed' class on the clicked element
+        event.target.classList.toggle('crossed');
 
-    // Store the crossed-out state in localStorage
-    const crossedOutItems = Array.from(listItems).filter((item) => item.classList.contains('crossed'));
-    localStorage.setItem('crossedOutItems', JSON.stringify(crossedOutItems.map((item) => item.textContent)));
-  });
-});
-
-// Retrieve crossed-out items from localStorage on page load
-window.addEventListener('DOMContentLoaded', () => {
-  const storedCrossedOutItems = localStorage.getItem('crossedOutItems');
-  if (storedCrossedOutItems) {
-    const crossedOutItems = JSON.parse(storedCrossedOutItems);
-    crossedOutItems.forEach((text) => {
-      const listItem = Array.from(listItems).find((item) => item.textContent === text);
-      if (listItem) {
-        listItem.classList.add('crossed');
-      }
-    });
-  }
-});
-// Sync crossed-out items across devices using a backend service
-// Please note: You'll need to implement API endpoints to handle these requests
-
-// Function to update the server with the latest crossed-out items
-function updateCrossedOutItemsOnServer(crossedOutItems) {
-  fetch('/updateCrossedOutItems', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ crossedOutItems }),
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (!data.success) {
-      console.error('Failed to update items on the server.');
+        // Save the updated list state (crossed items) to local storage
+        saveListState();
     }
-  })
-  .catch(error => {
-    console.error('Error updating crossed-out items:', error);
-  });
-}
-
-// Function to get the crossed-out items from the server when the page loads
-function getCrossedOutItemsFromServer() {
-  fetch('/getCrossedOutItems')
-    .then(response => response.json())
-    .then(data => {
-      if (data.crossedOutItems) {
-        data.crossedOutItems.forEach((text) => {
-          const listItem = Array.from(listItems).find((item) => item.textContent === text);
-          if (listItem) {
-            listItem.classList.add('crossed');
-          }
-        });
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching crossed-out items:', error);
-    });
-}
-
-// Call the function to sync crossed-out items on page load
-window.addEventListener('DOMContentLoaded', () => {
-  getCrossedOutItemsFromServer();
 });
+
+// Function to save the list state to local storage
+function saveListState() {
+    // Get all crossed items
+    var crossedItems = document.querySelectorAll('.crossed');
+    
+    // Create an array to store the crossed item names
+    var crossedNames = [];
+
+    // Iterate through crossed items and add their text content to the array
+    crossedItems.forEach(function (item) {
+        crossedNames.push(item.textContent);
+    });
+
+    // Convert the array to a JSON string and save it to local storage
+    localStorage.setItem('crossedNames', JSON.stringify(crossedNames));
+}
+
+// Function to load the list state from local storage
+function loadListState() {
+    // Get the crossed item names from local storage
+    var crossedNames = localStorage.getItem('crossedNames');
+
+    // If there are crossed names, iterate through them and cross out the corresponding items in the list
+    if (crossedNames) {
+        crossedNames = JSON.parse(crossedNames);
+        crossedNames.forEach(function (name) {
+            var crossedItem = document.querySelector('.list li:contains("' + name + '")');
+            if (crossedItem) {
+                crossedItem.classList.add('crossed');
+            }
+        });
+    }
+}
+
+// Call the loadListState function when the page is loaded to restore the state
+window.addEventListener('load', loadListState);
