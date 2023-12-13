0fb4d26666fea4c5dbdf26b2eec1c868712fa9cb
Roshan <roshanacha19@gmail.com>
2023-12-13 12:22:10 -0600

    Updated clear crossed list function

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs').promises; // Using promises for async file operations
 const bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
 const path = require('path');
@@ -8,43 +8,70 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => {
-    
-})
-
-app.get('/', (req, res) => {
-    readFile('./index.html', 'utf8', (err, html) => {
-        if (err) {
-            res.status(500).send('sorry, out of order');
-        }
-        res.send(html);
-    })
-    
+app.get('/', async (req, res) => {
+  try {
+    const html = await readFile('./index.html', 'utf8');
+    res.send(html);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Sorry, out of order');
+  }
 });
 
-// Store crossed-out items in-memory on the server
+// Store crossed-out items in a file on the server
+const filePath = path.join(__dirname, '/json/crossedItems.json');
+
 let crossedItems = [];
+
 const correctPassword = 'abcd1234z';
 const basicAuthMiddleware = basicAuth({
-    users: { 'admin': correctPassword }, // Use any username, but ensure the correct password is set
-    challenge: true,
-    unauthorizedResponse: 'Unauthorized'
+  users: { 'admin': correctPassword },
+  challenge: true,
+  unauthorizedResponse: 'Unauthorized'
 });
-app.use(express.json());
+
 // API endpoint to get and update crossed-out items
 app.route('/api/crossed-items')
-   .get((req, res) => {
-      res.json(crossedItems);
-   })
-   .post((req, res) => {
-      const { item } = req.body;
-      if (item && !crossedItems.includes(item)) {
-         crossedItems.push(item);
-      }
-      res.json(crossedItems);
-   });
-app.post('/api/crossed-items/clear', basicAuthMiddleware, (req, res) => {
-    crossedItems = []; // Clear crossed items
-    res.json({ success: true, message: 'Crossed items cleared' });
+  .get(async (req, res) => {
+    res.json(crossedItems);
+  })
+  .post(async (req, res) => {
+    const { item } = req.body;
+    if (item && !crossedItems.includes(item)) {
+      crossedItems.push(item);
+      await saveDataToFile();
+    }
+    res.json(crossedItems);
+  });
+
+// API endpoint to clear crossed-out items
+app.post('/api/crossed-items/clear', basicAuthMiddleware, async (req, res) => {
+  crossedItems = [];
+  await saveDataToFile();
+  res.json({ success: true, message: 'Crossed items cleared' });
 });
-app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000'));
+
+// Load data from the file on server start
+async function loadDataFromFile() {
+  try {
+    const data = await readFile(filePath, 'utf8');
+    crossedItems = JSON.parse(data);
+  } catch (err) {
+    // Ignore errors if the file doesn't exist or is invalid
+  }
+}
+
+// Save data to the file
+async function saveDataToFile() {
+  try {
+    await writeFile(filePath, JSON.stringify(crossedItems, null, 2), 'utf8');
+  } catch (err) {
+    console.error('Error saving data to file:', err);
+  }
+}
+
+// Load data from the file on server start
+loadDataFromFile().then(() => {
+  // Start the server after loading data
+  app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'));
+});
